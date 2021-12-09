111c57430182bbe2e522a39b70804c4094028ea8
Christian Osorio <ccdosorio11@gmail.com>
2021-12-09 14:32:47 -0600

    Primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent implements OnInit {
+
+  heroeBorrado: string = '';
+
+  heroes: string[] = ['Spiderman', 'Superman', 'Batman'];
+
+  constructor() {
+    console.log("Constructor");
+   }
+
+  ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+}
